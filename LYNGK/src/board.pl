30b75afbe65af2467793d89ab624ddbb37030e9a
dolfander <daniel.pmachado@hotmail.com>
2017-10-15 20:29:26 +0100

    latex support

--- LYNGK/src/board.pl
+++ LYNGK/src/board.pl
@@ -44,14 +44,41 @@ board3([  [.,.,e,.,p,.,a,.,.],
       ]).
 
 
-printBoard([L|Ls]) :-
-        printLine(L), nl,
-        printBoard(Ls).
-printBoard([]) :-
+%printBoard([L|Ls]) :-
+%        printLine(L), nl,
+%        printBoard(Ls).
+%printBoard([]) :-
+%        nl.
+%
+%printLine([X|Xs]) :-
+%        write(X), write(' '),
+%        printLine(Xs).
+%printLine([]).
+
+printBoard(Board) :-
+        printBlackLine,
+        printRowByRow(Board),
+        printBlackLine.
+
+printBlackLine :-
+        nl,
+        write('-------------------'),
         nl.
 
-printLine([X|Xs]) :-
-        write(X), write(' '),
-        printLine(Xs).
-printLine([]).
+printRowByRow([]).
+printRowByRow([Line|Rest]) :-
+        write('|'),
+        printSingleRow(Line),
+        printRowByRow(Rest).
+
+printSingleRow([Cell]):-
+        write(Cell),
+        write('|'),
+        printBlackLine.
+
+printSingleRow([Cell|More]):-
+        write(Cell),
+        write('|'),
+        printSingleRow(More).
+
 
