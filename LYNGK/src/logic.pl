895bf041025f4ed2d85c0cb5f5ff2323da593a9c
dolfander <daniel.pmachado@hotmail.com>
2017-11-11 00:28:26 +0000

    move almost working

--- LYNGK/src/logic.pl
+++ LYNGK/src/logic.pl
@@ -23,8 +23,52 @@ play(B,P1,P2,Colors) :-
 
         ).
 
+normalMove(B):-
+        selectPiece(B, Piece),
+        write('piece - '), write(Piece),
+        movePiece(B,B2,Piece),
+        B = B2,
+        printBoard(B).
 
 claimColor(Colors,NewColors,Player,NewPlayer):-
 	colorsMenu(Idx),
 	select(Idx,Colors,NewColors),
 	write(NewColors).
+
+selectPiece(B, P):-
+        write('Column number : '),
+        getColumnNumber(X),nl,
+        write('Line number : '),
+        getLineNumber(Y),
+        getPosition(B, 0, 0, X, Y, P).
+
+movePiece(B,B2,Piece):-
+        write('Column number : '),
+        getColumnNumber(X),nl,
+        write('Line number : '),
+        getLineNumber(Y),
+        setPosition(B, B2, 0, 0, X, Y, Piece).
+
+getColumnNumber(X) :-
+        X = _,
+        max = _,
+        read(X),
+        number(X),
+        X > -1 , X < 13.
+
+getColumnNumber(X) :-
+        write('Please pick a number between 0 and 13...'),
+        getColumnNumber(X).
+                
+
+getLineNumber(X) :-
+        X = _,
+        max = _,
+        read(X),
+        number(X),
+        X > -1 , X < 9.
+
+getLineNumber(X) :-
+        write('Please pick a number between 0 and 9...'),
+        getLineNumber(X).
+
