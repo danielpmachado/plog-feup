29e369a6aacf3d77b9ac63f3b6da19bd1840c2c5
dolfander <daniel.pmachado@hotmail.com>
2017-11-13 15:37:45 +0000

    Revert "possible moves"
    This reverts commit f4f65f6e5d66e071443d8290f5d3a440d7b26f36.

--- LYNGK/src/logic.pl
+++ LYNGK/src/logic.pl
@@ -8,8 +8,8 @@ startHvH:-
         createBoard(B,RandomB),
         random(0,2,R),
         (
-          R = 0 -> play(RandomB,[1],[2],[ivory,blue,red,green,black]);
-          play(RandomB,[2],[1],[ivory,blue,red,green,black])
+          R = 0 -> play(RandomB,[0,1],[0,2],[ivory,blue,red,green,black]);
+          play(RandomB,[0,2],[0,1],[ivory,blue,red,green,black])
         ).
 
 
@@ -25,7 +25,8 @@ play(Board,Player1,Player2,Colors) :-
 turn(Board,Player1,Player2,Colors,NewBoard,NewPlayer,NewColors):-
         clearScreen,
         printBoard(Board),nl,
-        printPlayersColors(Player1,Player2),
+        printPlayerInfo(Player1),
+        printPlayerInfo(Player2),
         printPlayer(Player1),
 
         makeMove(Complete,0,Board,Player1,Player2,Colors,NewBoard,NewPlayer,NewColors).
@@ -36,53 +37,121 @@ makeMove(Complete,Claimed,Board,P1,P2,Colors,NewBoard,NewPlayer,NewColors):-
         printMoveMenu,
         read(Option),
         (
-                Option = 1 -> normalMove(Complete,Board,P1,P2,Colors,NewBoard),NewPlayer = P1,NewColors = Colors;
-                Option = 2 -> write(2), Complete is 1;
+                Option = 1 -> normalMove(Complete,Board,P1,P2,Colors,NewBoard,NewPlayer),B1=Board,NP1 =P1, NC1 = Colors,Claimed1 is Claimed;
+                Option = 2 -> lyngkMove(Complete,Board,P1,NewBoard,NewPlayer),B1 = Board, NP1 = P1, NC1 = Colors,Claimed1 is Claimed;
                 Option = 3 -> Complete is 0,
-                              (Claimed \= 1  -> claimColor(Colors,NewColors,P1,NewPlayer), NewBoard = Board, Claimed1 is 1;
-                              write(' || You have already claimed a color this turn ||'),nl,nl, Claimed1 is Claimed);
+                              (Claimed \= 1  -> claimColor(Colors,NC1,P1,NP1), B1 = Board, Claimed1 is 1;
+                              write(' || You have already claimed a color this turn ||'),nl,nl, Claimed1 is 1);
                 Option = 4 -> halt;
 
                 write(' || Please choose a valid option. ||'),nl,nl,Complete is 0
         ),
-        (Complete \= 1 -> makeMove(Complete1,Claimed1,NewBoard,NewPlayer,P2,NewColors,NewBoard1,NewPlayer1,NewColors1); nl).
+        (Complete \= 1 -> makeMove(Complete1,Claimed1,B1,NP1,P2,NC1,NewBoard,NewPlayer,NewColors); nl).
 
 
-normalMove(Complete,Board,P1,P2,Colors,NewBoard):-
+lyngkMove(Complete,Board,Player,NewBoard,NewPlayer):-
+        getInitialPos(X,Y),
+        index(Board,Y,X,Piece),
+        nth0(0,Piece,Top),
+        ( \+(member(Top,Player)) ->
+            nl,Complete is 0, write(' || Ivalid piece for LYNGK move ||\n');
+            lyngkMenu(Complete,Board,X,Y,X,Y,Player,NewBoard,NewPlayer)
+            ).
+
+lyngkMenu(Complete,Board,X0,Y0,X1,Y1,Player,NewBoard,NewPlayer):-
+          printLYNGKMenu,
+          read(Option),
+          (
+            Option = 1 -> intermediateMove(Board,Player,X1,Y1,X,Y,Valid),
+                          (Valid = 0 ->nl,write(' || Invalid Position ||\n'), lyngkMenu(Complete,Board,X0,Y0,X1,Y1,Player,NewBoard,NewPlayer);
+                            lyngkMenu(Complete,Board,X0,Y0,X,Y,Player,NewBoard,NewPlayer));
+            Option = 2 -> finalMove(Complete,Board,Player,X0,Y0,X1,Y1,NewBoard,NewPlayer);
+
+            write(' || Please choose a valid option. ||'),nl,nl,lyngkMenu(Complete,Board,X1,Y1,Player,NewBoard,NewPlayer)
+          ).
+
+finalMove(Complete,Board,P1,X0,Y0,X1,Y1,NewBoard,NewPlayer):-
+          getFinalPos(X2,Y2),
+          checkValidLYNGK(Complete,Board,P1,X0,Y0,X1,Y1,X2,Y2),
+          (Complete = 1 ->  setPosition(Board, B, 0, 0, X0, Y0, [x]),
+                            index(Board,Y0,X0,InitPos),
+                            index(Board,Y2,X2,FinalPos),
+                            append(InitPos,FinalPos,Final),
+                            updatePlayer(B,P1,Final,X2,Y2,NewBoard,NewPlayer);
+                            write('')
+                         ).
+
+checkValidLYNGK(Complete,Board,P1,X0,Y0,X1,Y1,X2,Y2):-
+          (
+          checkPiece(Board,[],X0,Y0), nl,write(' -> valid piece\n'),
+          checkDiagonal(X1,X2,Y1,Y2),write(' -> valid diagonal\n'),
+          checkDiagonalPositions(Board,X1,X2,Y1,Y2), write(' -> valid diagonal positions\n'),
+          checkPosition(Board,X1,Y1),write(' -> valid initial position\n'),
+          checkPosition(Board,X2,Y2),write(' -> valid final position\n'),
+          checkFinalStack(Board,X0,Y0,X2,Y2), write(' -> valid final stack\n'),Complete is 1;
+          nl,write(' || Invalid Move ||'),nl,nl,Complete is 0).
+
+intermediateMove(Board,Player,X1,Y1,X2,Y2,Valid):-
+        nl,write(' Select Intermidiate position'),nl,
+        write(' Column number '),
+        getColumnNumber(X2),
+        write(' Line number '),
+        getLineNumber(Y2),
+        A is X1 - X2,
+        B is Y1 - Y2,
+        abs(A,Aabs), abs(B,Babs),
+        index(Board,Y2,X2,Piece),
+        nth0(0,Piece,Top),
+        (( Aabs==1,Babs ==1, member(Top,Player))->Valid is 1; Valid is 0).
+
+
+
+normalMove(Complete,Board,P1,P2,Colors,NewBoard,NewPlayer):-
         getInitialPos(X1,Y1),
         getFinalPos(X2,Y2),
         checkValidMove(Complete,Board,P1,P2,X1,Y1,X2,Y2),
         (Complete = 1 ->  setPosition(Board, B, 0, 0, X1, Y1, [x]),
                           index(Board,Y1,X1,InitPos),
                           index(Board,Y2,X2,FinalPos),
-                          nth0(0,InitPos,Top),
-                          append(FinalPos,[Top],Final),
-                          setPosition(B, NewBoard, 0, 0, X2, Y2, Final);
-                          NewBoard = Board
+                          append(InitPos,FinalPos,Final),
+                          updatePlayer(B,P1,Final,X2,Y2,NewBoard,NewPlayer);
+                          write('')
                        ).
+
+updatePlayer(B,P1,Final,X2,Y2,NewBoard,NewPlayer):-
+          length(Final,L),
+          nth0(0,Final,Top),
+          (
+            (L = 5, member(Top,P1))-> setPosition(B, NewBoard, 0, 0, X2, Y2, [x]),
+                                     nth0(0,P1,Points,Rest),
+                                     NewPoints is Points + 1,
+                                     append([NewPoints],Rest,NewPlayer);
+                                     setPosition(B, NewBoard, 0, 0, X2, Y2, Final),NewPlayer = P1
+          ).
+
 
 
 checkValidMove(Complete,Board,P1,P2,X1,Y1,X2,Y2):-
         (
-          checkTopPiece(Board,P2,X1,Y1), write(' -> valid top piece color\n'),
+          checkPiece(Board,P2,X1,Y1), nl,write(' -> valid piece\n'),
           checkDiagonal(X1,X2,Y1,Y2),write(' -> valid diagonal\n'),
           checkDiagonalPositions(Board,X1,X2,Y1,Y2), write(' -> valid diagonal positions\n'),
           checkPosition(Board,X1,Y1),write(' -> valid initial position\n'),
           checkPosition(Board,X2,Y2),write(' -> valid final position\n'),
+          checkNeutralTop(Board,P1,X1,Y1,X2,Y2), write(' -> valid neutral move\n'),
           checkFinalStack(Board,X1,Y1,X2,Y2), write(' -> valid final stack\n'),Complete is 1;
           nl,write(' || Invalid Move ||'),nl,nl,Complete is 0
         ).
 
-checkValidMove2(Complete,Board,P1,P2,X1,Y1,X2,Y2):-
+checkNeutralTop(Board,P1,X1,Y1,X2,Y2):-
+        index(Board,Y1,X1,Piece1),
+        nth0(0,Piece1,Top),
         (
-          %checkTopPiece(Board,P2,X1,Y1), 
-          %checkDiagonal(X1,X2,Y1,Y2),
-          %checkDiagonalPositions(Board,X1,X2,Y1,Y2), 
-          checkPosition(Board,X1,Y1),
-          checkPosition(Board,X2,Y2),
-         % checkFinalStack(Board,X1,Y1,X2,Y2),
-          Complete is 1;
-          Complete is 0
+          \+(member(Top,P1))-> index(Board,Y2,X2,Piece2),
+                               (is_list(Piece1) ->length(Piece1,L1);L1 is 1),
+                               (is_list(Piece2) ->length(Piece2,L2);L2 is 1),
+                               L2 =< L1;
+                               write('')
         ).
 
 checkFinalStack(Board,X1,Y1,X2,Y2):-
@@ -91,7 +160,11 @@ checkFinalStack(Board,X1,Y1,X2,Y2):-
         (is_list(Piece1) ->length(Piece1,L1);L1 is 1),
         (is_list(Piece2) ->length(Piece2,L2);L2 is 1),
         Total is L1 + L2,
-        Total =< 5.
+        Total =< 5,
+        append(Piece1,Piece2,Final),
+        delete(Final,white,F1),
+        delete(F1,white,F2),
+        is_set(F2).
 
 checkPosition(Board,X,Y):-
         index(Board,Y,X,Piece),
@@ -99,9 +172,11 @@ checkPosition(Board,X,Y):-
         \+(Top = '.'),
         \+(Top = x).
 
-checkTopPiece(Board,P2,X,Y):-
+checkPiece(Board,P2,X,Y):-
         index(Board,Y,X,Piece),
         nth0(0,Piece,Top),
+        length(Piece,L),
+        \+(L=5),
         \+(member(Top,P2)),
         \+(Top = white).
 
@@ -110,9 +185,8 @@ checkDiagonal(X1,X2,Y1,Y2):-
         A is X1abs - X2abs,
         abs(Y1,Y1abs), abs(Y2,Y2abs),
         B is Y1abs - Y2abs,
-        A == B.
-
-        
+        abs(A,Aabs),abs(B,Babs),
+        Aabs == Babs.
 
 checkDiagonalPositions(Board,X1,X2,Y1,Y2):-
         A is X2-X1,
@@ -122,7 +196,7 @@ checkDiagonalPositions(Board,X1,X2,Y1,Y2):-
         X is X1+Dx,
         Y is Y1+Dy,
         checkCell(Board, X,Y,Dx,Dy,X2).
-        
+
 checkCell(Board, X1,Y1,Dx,Dy,X2):-
         X1==X2,
         Top = _,
@@ -131,16 +205,16 @@ checkCell(Board, X1,Y1,Dx,Dy,X2):-
         \+(Top = x).
 
 checkCell(Board, X1,Y1,Dx,Dy,X2):-
-        Top = _, 
+        Top = _,
         index(Board,Y1,X1,Piece),
         nth0(0,Piece,Top),
         Top == x,
         X is X1+Dx,
         Y is Y1+Dy,
         checkCell(Board, X,Y,Dx,Dy,X2).
-        
-               
-        
+
+
+
 getInitialPos(X,Y):-
         nl,write(' Select initial position'),nl,
         write(' Column number '),
@@ -173,62 +247,56 @@ claimColor(Colors,FinalColors,Player,NewPlayer):-
           ).
 
 
-test(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+test(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         Y1 == 9,
         Y is Y1+1.
-       
-test(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+
+test(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         Y1 < 9,
-        forX1(Board,P1,P2,X1,Y1,X2,Y2,L,L1),
+        forX1(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1),
         Y is Y1+1,
-        test(Board,P1,P2,X1,Y,X2,Y2,L,L1).
+        test(Complete,Board,P1,P2,X1,Y,X2,Y2,L,L1).
 
 
-forX1(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forX1(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         X1 == 13,
         X is X1+1.
 
-forX1(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forX1(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         X1 < 13,
-        forY2(Board,P1,P2,X1,Y1,X2,Y2,L,L1),
+        forY2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1),
         X is X1+1,
-        forX1(Board,P1,P2,X,Y1,X2,Y2,L,L1).
+        forX1(Complete,Board,P1,P2,X,Y1,X2,Y2,L,L1).
 
 
 
-forY2(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forY2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         Y2 == 9,
         Y is Y2+1.
 
 
-forY2(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forY2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         Y2 < 9,
-        forX2(Board,P1,P2,X1,Y1,X2,Y2,L,L1),
+        forX2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1),
         Y is Y2+1,
-        forY2(Board,P1,P2,X1,Y1,X2,Y,L,L1).
+        forY2(Complete,Board,P1,P2,X1,Y1,X2,Y,L,L1).
 
 
-forX2(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forX2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         X2 == 13,
         X is X2+1,
-        forX2(Board,P1,P2,X1,Y1,X,Y2,L,L1).
+        forX2(Complete,Board,P1,P2,X1,Y1,X,Y2,L,L1).
 
 
-forX2(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+forX2(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
         X2 < 13,
-        %write(' X1-'),write(X1), write(' Y1-'),write(Y1),write(' X2-'),write(X2),write(' Y2-'),write(Y2),nl,
-        checkMove(Board,P1,P2,X1,Y1,X2,Y2,L,L1),
-        %write(' 2ndX1-'),write(X1), write(' Y1-'),write(Y1),write(' X2-'),write(X2),write(' Y2-'),write(Y2),nl,
+        write(' X1-'),write(X1), write(' Y1-'),write(Y1),write(' X2-'),write(X2),write(' Y2-'),write(Y2),nl,
+        %checkMove(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1),
         X is X2+1,
-        forX2(Board,P1,P2,X1,Y1,X,Y2,L1,_).
+        forX2(Complete,Board,P1,P2,X1,Y1,X,Y2,L1,_).
 
-forX2(_,_,_,_,_,_,_,_,_).
+forX2(_,_,_,_,_,_,_,_,_,_).
 
-checkMove(Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
-        checkValidMove2(Complete1,Board,P1,P2,X1,Y1,X2,Y2),
-        (Complete1 = 1 -> append([X1,Y1,X2,Y2],L,L1),
-                         write('valid L1-'),write(L1); 
-                          write(' ')).
-         
-
-
+checkMove(Complete,Board,P1,P2,X1,Y1,X2,Y2,L,L1):-
+        (checkValidMove(Complete,Board,P1,P2,X1,Y1,X2,Y2) ->  append([X1,Y1,X2,Y2],L,L1);
+         write('falhou')).
