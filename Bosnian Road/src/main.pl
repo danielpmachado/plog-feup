dda35ff2183298d3dd4de9c0e148e0d666ac8697
dolfander <daniel.pmachado@hotmail.com>
2017-12-20 01:53:04 +0000

    adjacent works

--- Bosnian Road/src/main.pl
+++ Bosnian Road/src/main.pl
@@ -18,3 +18,12 @@ start(Board):-
   reset_timer,
   solve(Board),
   print_time.
+
+
+send(Vars):-
+  Vars = [A1,A2,A3,B1,B3,C1,C2,C3],
+  domain(Vars,0,1),
+  %all_different(Vars),
+  A1 + A2 + A3 + B1 + B3 + C1 + C2 + C3 #= 5,
+/*  B2 #= 5,*/
+  labeling([], Vars).
